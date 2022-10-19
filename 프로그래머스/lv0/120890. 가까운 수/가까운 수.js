9de00d5d47199a488793c6c07cdd45ae44356ed2
yoorim <seou1225@naver.com>
2022-10-19 17:38:02 +0900

    [level 0] Title: 가까운 수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,6 +1,6 @@
 function solution(array, n) {
-    let minNum = Math.min(...array.map(num => Math.abs(num - n)));
     let assendingOrder = array.sort((a, b) => a - b);
+    let minNum = Math.min(...assendingOrder.map(num => Math.abs(num - n)));
 
     return assendingOrder.find(num => Math.abs(num - n) === minNum);
 }
