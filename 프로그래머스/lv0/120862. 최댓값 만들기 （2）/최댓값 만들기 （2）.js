af1c1041435c555eb3a3acee8b458da257806ca4
yoorim <seou1225@naver.com>
2022-10-23 14:46:06 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,9 +1,5 @@
 function solution(numbers) {
     numbers.sort((a, b) => a - b);
 
-    if(numbers[0] < 0 && numbers[1] < 0) {
-        return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 2] * numbers[numbers.length - 1]);
-    } else {
-        return numbers[numbers.length - 2] * numbers[numbers.length - 1];
-    }
+    return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 2] * numbers[numbers.length - 1]);
 }
