a7904c8b450ef55484fa5dec731018ef079264cc
yoorim <seou1225@naver.com>
2022-10-23 14:06:06 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,20 +1,9 @@
 function solution(numbers, direction) {
-    var answer = new Array(numbers.length);
-    
-    if(direction === "right") {
-        for (let i = 1; i < numbers.length; i++) {
-            answer[i] = numbers[i - 1];
-            if(i === numbers.length - 1) {
-                answer[0] = numbers[i];
-            }
-        }   
+    if (direction === "right") {
+        numbers.unshift(numbers.pop());
     } else {
-        for(let i = 0; i < numbers.length; i++) {
-            answer[i] = numbers[i + 1];
-            if(i === numbers.length - 1) {
-                answer[numbers.length - 1] = numbers[0];
-            }
-        }
+        numbers.push(numbers.shift());
     }
-    return answer;
+
+    return numbers;
 }
