a3efdc3685866a5b99424c777ae1fc7848dfa837
YoorimSeo <seou1225@naver.com>
2023-01-16 21:23:09 +0900

    [Bronze V] Title: 사칙연산, Time: 132 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
