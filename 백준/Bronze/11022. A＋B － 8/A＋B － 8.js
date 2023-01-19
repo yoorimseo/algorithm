cab76e0655b7b38655395f8e1d8ae477210cdb45
YoorimSeo <seou1225@naver.com>
2023-01-19 22:47:05 +0900

    [Bronze V] Title: A+B - 8, Time: 128 ms, Memory: 9528 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const line = parseInt(input[0]);
+
+for (let i = 1; i <= line; i++) {
+  const test = input[i].split(' ').map(Number);
+  console.log(`Case #${i}: ${test[0]} + ${test[1]} = ${test[0] + test[1]}`);
+}
