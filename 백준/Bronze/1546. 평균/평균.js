f937637db195194bd5e3a86008e16b11d78ab755
YoorimSeo <seou1225@naver.com>
2023-01-20 20:12:30 +0900

    [Bronze I] Title: 평균, Time: 140 ms, Memory: 9612 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.js
+++ 백준/Bronze/1546. 평균/평균.js
@@ -5,10 +5,6 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const num = parseInt(input[0]);
 const scores = input[1].split(' ').map(Number);
 const maxScore = Math.max(...scores);
-let result = 0;
-
-for (let i of scores) {
-  result += (i / maxScore) * 100;
-}
+let result = scores.reduce((acc, cur) => acc + (cur / maxScore) * 100, 0);
 
 console.log(result / num);
