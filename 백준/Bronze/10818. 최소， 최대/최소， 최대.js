8e6cf7db74c04ea8d5b337b9bcd3651324e922c5
YoorimSeo <seou1225@naver.com>
2023-01-20 19:25:52 +0900

    [Bronze III] Title: 최소, 최대, Time: 436 ms, Memory: 96004 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const arr = input[1].split(' ').map(Number);
+
+console.log(Math.min(...arr), Math.max(...arr));
