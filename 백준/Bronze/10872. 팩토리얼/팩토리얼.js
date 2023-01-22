0b70427089c62bc91af0beb96514347530f25c4c
YoorimSeo <seou1225@naver.com>
2023-01-22 22:05:00 +0900

    [Bronze V] Title: 팩토리얼, Time: 144 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+let answer = 1;
+
+for (let i = 1; i <= parseInt(input); i++) {
+  answer *= i;
+}
+
+console.log(answer);
