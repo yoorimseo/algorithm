c516843a0677e792ddc58442567cff7205e0f42b
YoorimSeo <seou1225@naver.com>
2023-01-26 21:41:22 +0900

    [level 0] Title: 모스부호 (1), Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,17 @@
+function solution(letter) {
+    const morse = { 
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    }
+    let answer = '';
+    const letterArr = letter.split(' ');
+    
+    for (let s of letterArr) {
+        answer += morse[s];
+    }
+    
+    return answer;
+}
