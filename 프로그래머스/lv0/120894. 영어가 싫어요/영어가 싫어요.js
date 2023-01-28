e79ec9a04e90e3f8c53ca71f68e912e495f332c5
YoorimSeo <seou1225@naver.com>
2023-01-28 23:06:43 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,14 @@
+function solution(numbers) {
+    const nums = {
+        'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+        'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'zero': 0
+    }
+    
+    let answer = numbers;
+    
+    for (let i in nums) {
+        answer = answer.replaceAll(i, nums[i]);
+    }
+    
+    return +answer;
+}
