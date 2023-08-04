38b9e3403e1e4c39a70a0cbb9c0c14505a1d6fd5
YoorimSeo <seou1225@naver.com>
2023-08-04 15:28:46 +0900

    [Silver IV] Title: DNA, Time: 88 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1969. DNA/DNA.py
+++ 백준/Silver/1969. DNA/DNA.py
@@ -1,33 +1,33 @@
-n, m = list(map(int, input().strip().split()))
-dna_list = [input().strip() for _ in range(n)]
-res_cnt = 0
-res_str = ''
+N, M = map(int, input().strip().split())
+DNA = [input().strip() for i in range(N)]
+res = ''
+cnt = 0
 
-for i in range(m):
-    arr = [0, 0, 0, 0] # A C G T 순
+for m in range(M):
+    arr = [0, 0, 0, 0] # A, C, G, T
 
-    for j in range(n):
-        if dna_list[j][i] == 'A':
+    for n in range(N):
+        if DNA[n][m] == 'A':
             arr[0] += 1
-        elif dna_list[j][i] == 'C':
+        elif DNA[n][m] == 'C':
             arr[1] += 1
-        elif dna_list[j][i] == 'G':
+        elif DNA[n][m] == 'G':
             arr[2] += 1
-        elif dna_list[j][i] == 'T':
+        elif DNA[n][m] == 'T':
             arr[3] += 1
 
     max_idx = arr.index(max(arr))
 
     if max_idx == 0:
-        res_str += 'A'
+        res += 'A'
     elif max_idx == 1:
-        res_str += 'C'
+        res += 'C'
     elif max_idx == 2:
-        res_str += 'G'
+        res += 'G'
     elif max_idx == 3:
-        res_str += 'T'
-        
-    res_cnt += sum(arr) - max(arr)
+        res += 'T'
 
-print(res_str)
-print(res_cnt)
+    cnt += sum(arr) - max(arr)
+
+print(res)
+print(cnt)
