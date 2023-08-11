bd720bc502dc205f281407b0573c7a84b820779b
YoorimSeo <seou1225@naver.com>
2023-08-11 20:49:13 +0900

    [Bronze III] Title: 수학적 호기심, Time: 4928 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9094. 수학적 호기심/수학적 호기심.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for _ in range(T):
+    n, m = map(int, input().split())
+    cnt = 0
+
+    for a in range(1, n - 1):
+        for b in range(a + 1, n):
+            if (a ** 2 + b ** 2 + m) % (a * b) == 0:
+                cnt += 1
+
+    print(cnt)
