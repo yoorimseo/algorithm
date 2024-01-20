acb8cffe002dbf637cff3edc170a578730c29b5e
YoorimSeo <seou1225@naver.com>
2024-01-20 19:52:08 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split())
+arr = list(range(1, N+1))
+idx = 0
+res = []
+
+while len(arr) != 0:
+    idx += K-1
+    idx %= len(arr)
+    res.append(arr.pop(idx))
+
+print(f'<{", ".join(map(str, res))}>')
