a4f2a6508c6622e4554311f6a0b0d632f4910fa6
YoorimSeo <seou1225@naver.com>
2024-03-06 23:45:48 +0900

    [Silver II] Title: 좌표 압축, Time: 1188 ms, Memory: 282104 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+arr = list(map(int, input().split()))
+
+sorted_arr = sorted(set(arr))
+dic = {sorted_arr[i]:i for i in range(len(sorted_arr))}
+
+for i in arr:
+    print(dic[i], end=" ")
