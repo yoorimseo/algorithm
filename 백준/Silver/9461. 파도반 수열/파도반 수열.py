2147894b1a21cb8f5c1e4a3d1efbd5220efdde86
YoorimSeo <seou1225@naver.com>
2024-05-12 21:12:00 +0900

    [Silver III] Title: 파도반 수열, Time: 124 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,11 @@
+T = int(input())
+
+dp = [0, 1, 1, 1, 2, 2]
+def solve():
+    for i in range(6, 100+1):
+        dp.append(dp[i-1] + dp[i-5])
+
+solve()
+for _ in range(T):
+    N = int(input())
+    print(dp[N])
