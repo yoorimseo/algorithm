65778200ef00f408cd7fc66465266421c3790ed2
YoorimSeo <seou1225@naver.com>
2024-07-07 19:08:27 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 440 ms, Memory: 169768 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,9 +1,19 @@
-S = input()
+import sys
+input = sys.stdin.readline
+
+S = input().rstrip()
+array = [[0] * 26 for _ in range(len(S)+1)]
+
+for i in range(1, len(S)+1):
+    for j in range(26):
+        if ord(S[i-1])-97 == j:
+            array[i][j] = array[i-1][j] + 1
+        else:
+            array[i][j] = array[i-1][j]
+
+
 q = int(input())
-
 for _ in range(q):
-    a, l, r = input().split(' ')
-    l, r = int(l), int(r)
-    res = list(filter(lambda s: s == a, S[l:r+1]))
-
-    print(len(res))
+    a, l, r = input().split()
+    a, l, r = ord(a)-97, int(l), int(r)
+    print(array[r+1][a]-array[l][a])
