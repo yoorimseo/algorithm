d6396f58a9cd4a7dcfef38e50fb1faf64455d98f
YoorimSeo <seou1225@naver.com>
2024-09-21 18:48:05 +0900

    [level 2] Title: 올바른 괄호, Time: 15.06 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,16 @@
+def solution(s):
+    stack = []
+    
+    for i in s:
+        if len(stack) == 0:
+            stack.append(i)
+        else:
+            if stack[-1] == '(' and i == ')':
+                stack.pop()
+            else:
+                stack.append(i)
+    
+    if len(stack) > 0:
+        return False
+    
+    return True
