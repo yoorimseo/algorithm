de5c77ae9d7ba425376ddddb6e417148136cb76b
YoorimSeo <seou1225@naver.com>
2024-10-21 22:37:35 +0900

    [Gold IV] Title: 동전 1, Time: 108 ms, Memory: 111512 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+coins = sorted([int(input()) for _ in range(n)])
+dp = [0] * (k + 1)
+dp[0] = 1
+
+for c in coins:
+    for i in range(c, k + 1):
+        dp[i] += dp[i - c]
+
+print(dp[k])
