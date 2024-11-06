74f525b5971d774388e0afc00d2679f699cf0355
YoorimSeo <seou1225@naver.com>
2024-11-06 21:46:15 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -1,8 +1,4 @@
-import sys
+N = int(input())
 
-num = int(sys.stdin.readline().strip())
-blank = num
-
-for i in range(1, num + 1):
-  blank -= 1
-  print(' ' * blank + '*' * i)
+for i in range(1, N+1):
+    print(' ' * (N - i) + '*' * i)
