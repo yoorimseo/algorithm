3a5a82494763d59b43f8b0376d0c838e854c27d4
YoorimSeo <seou1225@naver.com>
2024-11-08 21:03:16 +0900

    [level 1] Title: 소수 만들기, Time: 18.45 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -4,16 +4,15 @@ def is_prime(n):
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
+    
     return True
 
 def solution(nums):
     arr = list(combinations(nums, 3))
     answer = 0
-    
+
     for i in arr:
-        cmp = sum(i)
-
-        if is_prime(cmp):
+        if is_prime(sum(i)):
             answer += 1
-
+    
     return answer
