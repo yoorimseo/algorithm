3e354eb19e3d248c701f016c6f26d8d370aea460
YoorimSeo <seou1225@naver.com>
2025-01-16 10:24:57 +0900

    [level 0] Title: 배열 비교하기, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -1,14 +1,14 @@
 function solution(arr1, arr2) {
-    if (arr1.length !== arr2.length) {
-        return arr1.length < arr2.length ? -1 : 1
-    } else {
+    if (arr1.length === arr2.length) {
         let sum1 = arr1.reduce((acc, cur) => acc + cur, 0)
         let sum2 = arr2.reduce((acc, cur) => acc + cur, 0)
         
         if (sum1 === sum2) {
             return 0
+        } else if (sum1 > sum2) {
+            return 1
         }
-        
-        return sum1 < sum2 ? -1 : 1
+        return -1
     }
+    return arr1.length > arr2.length ? 1 : -1
 }
