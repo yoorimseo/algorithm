806a643503d565f4cc2ed8ab6a7e3a287093489e
YoorimSeo <seou1225@naver.com>
2025-01-16 09:57:55 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.js
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.js
@@ -1,7 +1,8 @@
 function solution(num_str) {
-    const answer = num_str.split('')
-                          .map(i => Number(i))
-                          .reduce((acc, cur) => acc + cur, 0)
+    let answer = num_str
+                .split('')
+                .map(i => parseInt(i))
+                .reduce((acc, cur) => acc + cur, 0)
     
     return answer
 }
