e9f4b8071f128bdaf860986343deb20142fcea2c
YoorimSeo <seou1225@naver.com>
2025-01-17 09:28:34 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,10 +1,14 @@
 -- 코드를 입력하세요
-SELECT
-    CONCAT('/home/grep/src/', F.BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT)
-    AS FILE_PATH
-FROM USED_GOODS_FILE F
-WHERE F.BOARD_ID = (SELECT B.BOARD_ID
-                    FROM USED_GOODS_BOARD B
-                    WHERE VIEWS = (SELECT MAX(VIEWS) FROM USED_GOODS_BOARD))
-ORDER BY F.FILE_ID DESC
+# USED_GOODS_BOARD와 USED_GOODS_FILE 테이블에서 조회수가 가장 높은 중고거래 게시물에 대한 첨부파일 경로를 조회
+# 첨부파일 경로는 FILE ID를 기준으로 내림차순 정렬
+# 기본적인 파일경로는 /home/grep/src/, 게시글 ID를 기준으로 디렉토리가 구분되고, 파일이름은 파일 ID, 파일 이름, 파일 확장자로 구성되도록 출력
+# 조회수가 가장 높은 게시물은 하나만 존재합니다.
 
+SELECT
+    CONCAT('/home/grep/src/', BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_FILE
+WHERE BOARD_ID IN (SELECT BOARD_ID
+                   FROM USED_GOODS_BOARD
+                   WHERE VIEWS = (SELECT MAX(VIEWS)
+                                  FROM USED_GOODS_BOARD))
+ORDER BY FILE_ID DESC
