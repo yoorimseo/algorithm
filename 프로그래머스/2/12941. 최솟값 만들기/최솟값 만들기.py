9b8bba63e5ac849bbee324f59e9611c8e501d9c1
YoorimSeo <seou1225@naver.com>
2025-01-28 22:50:53 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.23 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,14 +1,10 @@
-def solution(A,B):
-    answer = 0
-
-    if sum(A) < sum(B):
-        A.sort()
-        B.sort(reverse=True)
-    else:
-        A.sort(reverse=True)
-        B.sort()
-
-    for i in range(len(A)):
-        answer += (A[i] * B[i])
+def solution(A,B):    
+    A.sort()
+    B.sort(reverse=True)
     
+    answer = 0
+    
+    for [i, j] in zip(A, B):
+        answer += i * j
+        
     return answer
