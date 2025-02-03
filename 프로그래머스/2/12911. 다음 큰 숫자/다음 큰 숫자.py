628f67228cb0eb1208545a0b0e531dea93fcf148
YoorimSeo <seou1225@naver.com>
2025-02-03 15:38:14 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,10 +1,8 @@
 def solution(n):
-    cnt_n = len(list(filter(lambda x: x == '1', format(n, 'b'))))
-    answer = 0
-
-    for i in range(n+1, 1000000):
-        bin_i = format(i, 'b')
-        cnt_i = len(list(filter(lambda x: x == '1', bin_i)))
-
-        if cnt_i == cnt_n:
-            return int(bin_i, 2)
+    c = n + 1
+    
+    while True:
+        if bin(c).count('1') == bin(n).count('1'):
+            return c
+        
+        c += 1
