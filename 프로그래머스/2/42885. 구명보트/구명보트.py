681a40f42bc24d0312d4857b019a0cf9a038687e
YoorimSeo <seou1225@naver.com>
2025-02-05 14:43:08 +0900

    [level 2] Title: 구명보트, Time: 9.00 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,13 +1,15 @@
 def solution(people, limit):
-    people.sort()
-    left = 0
-    right = len(people) - 1
-    boats = 0
+    first = 0
+    last = len(people) - 1
+    answer = 0
 
-    while left <= right:
-        if people[left] + people[right] <= limit:
-            left += 1
-        right -= 1
-        boats += 1
+    people.sort(reverse = True)
 
-    return boats
+    while first <= last:
+        if people[first] + people[last] <= limit:
+            last -= 1
+
+        first += 1
+        answer += 1
+
+    return answer
