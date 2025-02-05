39c4d693a8e0c4ef007a99a85ffc9564d346b983
YoorimSeo <seou1225@naver.com>
2025-02-05 15:20:45 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,16 +1,7 @@
 def solution(n):
-    MOD = 1234567
+    a, b = 0, 1
     
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 2
+    for i in range(2, n+1):
+        a, b = b, a + b
     
-    dp = [0] * (n + 1)
-    dp[1] = 1
-    dp[2] = 2
-    
-    for i in range(3, n + 1):
-        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD
-    
-    return dp[n]
+    return (a + b) % 1234567
