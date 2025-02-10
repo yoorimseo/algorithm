f39ffa6c60279cef3b232ffbd6b6d60b7f5d4fff
YoorimSeo <seou1225@naver.com>
2025-02-10 20:46:19 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,12 +1,10 @@
 def solution(n, words):
-    used_words = set()
-    used_words.add(words[0])
+    num, cnt = 0, 0
+    cmp = words[0]
 
     for i in range(1, len(words)):
-        if words[i] in used_words or words[i][0] != words[i-1][-1]:
-            person = (i % n) + 1
-            turn = (i // n) + 1
-            return [person, turn]
-        used_words.add(words[i])
+        if  words[:i+1].count(words[i]) > 1 or cmp[-1] != words[i][0]:
+            return [i % n + 1, i // n + 1]
+        cmp = words[i]
 
     return [0, 0]
